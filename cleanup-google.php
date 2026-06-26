d7d07bad4ab31ad755c8cde1de4b7822c39cd3fe
Chandan2909-commits <chandan153377@gmail.com>
2026-06-27 01:32:42 +0530

    fix: add debug logging to cleanup-google.php for vendor patching

--- cleanup-google.php
+++ cleanup-google.php
@@ -39,9 +39,72 @@ foreach ($dirIterator as $item) {
 echo "Google API client services cleanup completed. Deleted $deletedDirs directories and $deletedFiles service files.\n";
 
 // --- Patch ViserLab License Check ---
-$onumotiFile = __DIR__ . '/vendor/laramin/utility/src/Onumoti.php';
-if (file_exists(dirname($onumotiFile))) {
-    echo "Patching Onumoti.php to bypass license check...\n";
+$vendorDir = __DIR__ . '/vendor';
+if (is_dir($vendorDir)) {
+    echo "Vendor dir exists.\n";
+    $laraminDir = $vendorDir . '/laramin';
+    if (is_dir($laraminDir)) {
+        echo "laramin folder exists: " . implode(', ', array_diff(scandir($laraminDir), ['.', '..'])) . "\n";
+        $utilityDir = $laraminDir . '/utility';
+        if (is_dir($utilityDir)) {
+            echo "utility folder exists: " . implode(', ', array_diff(scandir($utilityDir), ['.', '..'])) . "\n";
+            $srcDir = $utilityDir . '/src';
+            if (is_dir($srcDir)) {
+                echo "src folder exists: " . implode(', ', array_diff(scandir($srcDir), ['.', '..'])) . "\n";
+                
+                $onumotiFile = $srcDir . '/Onumoti.php';
+                $onumotiCode = '<?php
+
+namespace Laramin\Utility;
+
+use App\Lib\CurlRequest;
+use App\Models\GeneralSetting;
+
+class Onumoti{
+
+    public static function getData(){
+        // license check removed
+    }
+
+    public static function mySite($site,$className){
+        // license check removed
+    }
+}';
+                echo "Writing patched Onumoti.php...\n";
+                file_put_contents($onumotiFile, $onumotiCode);
+                
+                $helpmateFile = $srcDir . '/Helpmate.php';
+                $helpmateCode = '<?php
+
+namespace Laramin\Utility;
+
+use App\Models\GeneralSetting;
+
+class Helpmate{
+
+    public static function sysPass(){
+        return true;
+    }
+
+    public static function appUrl(){
+        $current = @$_SERVER[\'REQUEST_SCHEME\'] ?? \'http\' . \'://\' . $_SERVER[\'HTTP_HOST\'] . $_SERVER[\'REQUEST_URI\'];
+        $url = substr($current, 0, -9);
+        return  $url;
+    }
+}';
+                echo "Writing patched Helpmate.php...\n";
+                file_put_contents($helpmateFile, $helpmateCode);
+            }
+        }
+    }
+} else {
+    echo "Vendor dir NOT found!\n";
+}
+
+// Local test environment patch
+$localOnumoti = __DIR__ . '/democrm/Files/core/vendor/laramin/utility/src/Onumoti.php';
+if (file_exists($localOnumoti)) {
+    echo "Patching local environment Onumoti.php...\n";
     $onumotiCode = '<?php
 
 namespace Laramin\Utility;
@@ -59,19 +122,11 @@ class Onumoti{
         // license check removed
     }
 }';
-    file_put_contents($onumotiFile, $onumotiCode);
-} else {
-    // Check if we are running in local test environment where vendor is in democrm/Files/core/vendor
-    $onumotiFileLocal = __DIR__ . '/democrm/Files/core/vendor/laramin/utility/src/Onumoti.php';
-    if (file_exists($onumotiFileLocal)) {
-        echo "Patching local Onumoti.php...\n";
-        file_put_contents($onumotiFileLocal, $onumotiCode);
-    }
+    file_put_contents($localOnumoti, $onumotiCode);
 }
-
-$helpmateFile = __DIR__ . '/vendor/laramin/utility/src/Helpmate.php';
-if (file_exists(dirname($helpmateFile))) {
-    echo "Patching Helpmate.php to bypass license check...\n";
+$localHelpmate = __DIR__ . '/democrm/Files/core/vendor/laramin/utility/src/Helpmate.php';
+if (file_exists($localHelpmate)) {
+    echo "Patching local environment Helpmate.php...\n";
     $helpmateCode = '<?php
 
 namespace Laramin\Utility;
@@ -90,13 +145,7 @@ class Helpmate{
         return  $url;
     }
 }';
-    file_put_contents($helpmateFile, $helpmateCode);
-} else {
-    $helpmateFileLocal = __DIR__ . '/democrm/Files/core/vendor/laramin/utility/src/Helpmate.php';
-    if (file_exists($helpmateFileLocal)) {
-        echo "Patching local Helpmate.php...\n";
-        file_put_contents($helpmateFileLocal, $helpmateCode);
-    }
+    file_put_contents($localHelpmate, $helpmateCode);
 }
 
 function deleteDirectory($dirPath) {
